eb672472790d2f3c72c60878c56695e01f38fb5b
popote1 <yvan.bladet@gmail.com>
2025-10-19 17:31:47 +0200

    MinorModifications

--- Assets/Scripts/DamageElement.cs
+++ Assets/Scripts/DamageElement.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 public class DamageElement : MonoBehaviour {
     [SerializeField]private int _damages;
     [SerializeField] private bool _damagesMonsters;
+    [SerializeField] private GameObject _prfHit;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.transform.CompareTag("Player")) {
             other.gameObject.GetComponent<PlayerController2D>().TakeDamage(_damages, transform.position);
+            if (_prfHit != null) Instantiate(_prfHit, other.transform.position, Quaternion.identity);
         }
 
+        if (!_damagesMonsters) return;
         if (other.gameObject.GetComponent<Monster>() != null) {
             other.gameObject.GetComponent<Monster>().TakeDamage(_damages, transform.position);
+            if (_prfHit != null) Instantiate(_prfHit, other.transform.position, Quaternion.identity);
         }
     }
 }
